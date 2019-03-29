c861f28ab60c13c5007115a3dd3c80a22d07ad71
KangYh <34295007+0-0MrLonely@users.noreply.github.com>
2019-03-29 20:42:56 +0800

    1

--- app/src/main/java/com/example/fangdou2/fragment/ListViewFragment.java
+++ app/src/main/java/com/example/fangdou2/fragment/ListViewFragment.java
@@ -1,33 +1,37 @@
 package com.example.fangdou2.fragment;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fangdou2.MyDrawerLayout;
+import com.example.fangdou2.MyListView;
 import com.example.fangdou2.R;
 import com.example.fangdou2.Tts;
-import com.example.fangdou2.adapter.MyAdapter;
-import com.example.fangdou2.bean.ItemBean;
+import com.example.fangdou2.adapter.RecordAdapter;
+import com.example.fangdou2.bean.RecordItemBean;
 import com.example.fangdou2.utils.RecordingItem;
 import com.iflytek.cloud.SpeechConstant;
-import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.SpeechUtility;
-
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,13 +40,13 @@ import java.util.List;
 import static android.content.Context.MODE_PRIVATE;
 
 
-public class ListViewFragment extends Fragment implements MyAdapter.Callback
+public class ListViewFragment extends Fragment implements RecordAdapter.Callback
 {
     private View view;
-    private ListView listView;
+    private MyListView listView;
     private MediaPlayer mediaPlayer;
     private TextView textView;
-    private List<ItemBean> itemBeanList;
+    private List<RecordItemBean> recordItemBeanList;
     private String app_id = "5c6e22da";
     private MyDrawerLayout draw;
     private RadioGroup group;
@@ -54,6 +58,7 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
     private Menu menu;
     private Tts tts;
     private String voicer = "xiaoyan";
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Nullable
     @Override
@@ -70,7 +75,7 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
 
         } else
         {
-            view = inflater.inflate(R.layout.medialistview, null);
+            view = inflater.inflate(R.layout.fragment_listview, null);
         }
         StringBuffer param = new StringBuffer();
         param.append("appid=" + app_id);
@@ -87,8 +92,8 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
 
     public void initSide()
     {
-        drawerLayout = (MyDrawerLayout) view.findViewById(R.id.listView_Fragment);
-        navigationView = (NavigationView) view.findViewById(R.id.nav);
+        drawerLayout = view.findViewById(R.id.listView_Fragment);
+        navigationView = view.findViewById(R.id.nav);
         mCloudVoicersEntries = view.getResources().getStringArray(R.array.voicer_cloud_entries);
         mCloudVoicersValue = view.getResources().getStringArray(R.array.voicer_cloud_values);
         menu = navigationView.getMenu();
@@ -101,11 +106,51 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item)
             {
-                //item.setChecked(true);
+                //recorditem.setChecked(true);
                 voicer = mCloudVoicersValue[item.getItemId()];
-                Toast.makeText(view.getContext(), voicer, Toast.LENGTH_LONG).show();
+                Toast.makeText(view.getContext(), "现在是" + mCloudVoicersEntries[item.getItemId()], Toast.LENGTH_SHORT).show();
                 drawerLayout.closeDrawer(navigationView);
                 return true;
+            }
+        });
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        setHasOptionsMenu(true);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle("方逗");
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, 0, 0)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView)
+            {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (drawerLayout.isDrawerOpen(GravityCompat.END))
+                {
+                    drawerLayout.closeDrawers();
+                } else
+                {
+                    drawerLayout.openDrawer(Gravity.END);
+                }
             }
         });
     }
@@ -114,32 +159,32 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
     {
         menu = navigationView.getMenu();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu_language, menu);
         super.onCreateOptionsMenu(menu, inflater);
 
     }
 
     public void initView()
     {
-        itemBeanList = new ArrayList<>();
+        recordItemBeanList = new ArrayList<>();
         for (int i = 0; i < 20; i++)
         {
             if (i % 2 == 0)
             {
-                itemBeanList.add(new ItemBean("嘿嘿嘿嘿嘿嘿嘿", R.raw.test));
+                recordItemBeanList.add(new RecordItemBean("嘿嘿嘿嘿嘿嘿嘿", R.raw.test));
             } else
-                itemBeanList.add(new ItemBean("哈哈哈哈哈哈哈", R.raw.test));
+                recordItemBeanList.add(new RecordItemBean("哈哈哈哈哈哈哈", R.raw.test));
             //在此处添加文字与音频文件
         }
-        listView = (ListView) view.findViewById(R.id.listView);
-        listView.setAdapter(new MyAdapter(itemBeanList, getLayoutInflater(), this));
+        listView = view.findViewById(R.id.listView);
+        listView.setAdapter(new RecordAdapter(recordItemBeanList, getLayoutInflater(), this));
 
     }
 
     @Override
     public void click(View v)
     {
-        textView = (TextView) v.findViewById(R.id.item_text);
+        textView = v.findViewById(R.id.item_text);
         switch (v.getId())
         {
             case R.id.item_text:
@@ -153,7 +198,7 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
                     {
                         if (mediaPlayer == null)
                         {
-                            mediaPlayer = MediaPlayer.create(view.getContext(), itemBeanList.get((Integer) v.getTag()).resourceId);
+                            mediaPlayer = MediaPlayer.create(view.getContext(), recordItemBeanList.get((Integer) v.getTag()).resourceId);
                             mediaPlayer.prepare();
                         }
                         mediaPlayer.start();
@@ -198,7 +243,7 @@ public class ListViewFragment extends Fragment implements MyAdapter.Callback
                 fragmentPlay.show(getFragmentManager(), PlaybackDialogFragment.class.getSimpleName());
                 break;
             case R.id.img_natural:
-                tts = new Tts(v, itemBeanList.get((Integer) v.getTag()).lrc, voicer);
+                tts = new Tts(v, recordItemBeanList.get((Integer) v.getTag()).lrc, voicer);
                 tts = null;
             default:
                 break;
--- app/src/main/java/com/example/fangdou2/fragment/MapFragment.java
+++ app/src/main/java/com/example/fangdou2/fragment/MapFragment.java
@@ -1,23 +1,125 @@
 package com.example.fangdou2.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.fangdou2.MyDrawerLayout;
+import com.example.fangdou2.MyListView;
 import com.example.fangdou2.R;
+import com.example.fangdou2.adapter.LanguageAdapter;
+import com.example.fangdou2.bean.LanguageItemBean;
 
-public class MapFragment extends Fragment
+import java.util.ArrayList;
+
+public class MapFragment extends Fragment implements LanguageAdapter.Callback
 {
-    View view;
+    private View view;
+    private String language_item[];
+    private ArrayList<LanguageItemBean> arrayList;
+    public static MyListView listView;
+    private int locate;
+    private InfoFragment infoFragment;
+    private MyDrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
+        if (view != null)
+        {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+            {
+                parent.removeView(view);
+            }
+
+        } else
+        {
+            view = inflater.inflate(R.layout.fragment_map, null);
+        }
+
+        initView();
         return view;
     }
+
+    public void initView()
+    {
+        //initItem();
+        language_item = new String[8];
+        language_item = view.getResources().getStringArray(R.array.language_item);
+        arrayList = new ArrayList<>();
+        for (int i = 0; i < language_item.length; i++)
+        {
+            arrayList.add(new LanguageItemBean(language_item[i]));
+        }
+        listView = view.findViewById(R.id.language_listView);
+        listView.setAdapter(new LanguageAdapter(arrayList, getLayoutInflater(), this));
+        initLeftNavi();
+    }
+
+
+    @Override
+    public void click(View v)
+    {
+        infoFragment = new InfoFragment("示例文字", "http://www.baidu.com");
+        getFragmentManager().beginTransaction().replace(R.id.infoFragment, infoFragment).addToBackStack(null).commit();
+
+    }
+
+    private void initLeftNavi()
+    {
+        drawerLayout = view.findViewById(R.id.leftDraw);
+        navigationView = view.findViewById(R.id.nav);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                Toast.makeText(getContext(), menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        /*下面是toolbar的设置，包括动画*/
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        setHasOptionsMenu(true);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle("方逗");
+        toolbar.setTitleTextColor(Color.WHITE);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, 0, 0)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView)
+            {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+
 }
